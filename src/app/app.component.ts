8edba0a4a417cd4e18e994ffb3edece7309ad4e9
Gerald Ruiz <gerald.ruiz@accenture.com>
2018-06-18 20:49:45 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { OverlayContainer } from '@angular/material';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'amte-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'amte';
+export class AppComponent implements OnInit{
+  title = 'Theming around';
+  theme = 'my-theme';
+  themeClass: string;
+
+    constructor(
+      private overlayContainer: OverlayContainer
+    ) {}
+
+    ngOnInit(): void {
+      this.overlayContainer.themeClass = this.theme;
+    }
+
+    onThemeChange () {
+      this.overlayContainer.themeClass = this.theme;
+    }
+
+
 }
+
+// {
+//   "workbench.welcome.enabled": false,
+//   "workbench.iconTheme": "vscode-icons",
+//   "vsicons.projectDetection.autoReload": true,
+//   "workbench.colorTheme": "Dracula Dark Vibrant",
+//   "editor.minimap.enabled": false,
+//   "editorGroupHeader.tabsBackground": "#000",
+//   "window.zoomLevel": 1,
+//   "editor.fontFamily": "Source Code Pro",
+//   "editor.fontSize": 15,
+//   "workbench.colorCustomizations":{
+//       // "statusBar.background": "#07bb25",
+//       "activityBar.background":"#000",
+//       "panel.background": "#555555",
+//       "sideBar.background": "#000",
+//       "editor.background": "#000",
+//       "editorLineNumber.foreground": "#555",
+//       "editorGroupHeader.tabsBackground":"#000",
+//       "tab.inactiveBackground": "#000",
+//   }
+
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatSelectModule
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
